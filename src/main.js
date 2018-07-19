b8ca7f594a37a229c8c932bc1d0578879ae1608e
rocco <rocco.mormont@gmail.com>
2018-07-20 02:07:09 +0800

    :white_check_mark: add demos

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import AutoStorage from "./lib/index";
+import router from "./router";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
@@ -10,5 +11,6 @@ Vue.use(ElementUI);
 Vue.use(AutoStorage);
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
