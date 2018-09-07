b859671d241e01b472f3d92f780ddb1a1b529070
岩 <rocco.mormont@gmail.com>
2018-09-07 13:41:08 +0800

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,33 @@
-interface Storage {
+import _Vue, { ComponentOptions } from 'vue';
+import { VueConstructor } from 'vue/types/vue';
+
+export interface Storage {
   getItem: (key: string) => string;
   setItem: (key: string, value: string) => void;
   removeItem: (key: string) => void;
   clear: () => void;
 }
 
-interface Options {
+export interface Options {
   debounce?: number;
   storage?: Storage;
 }
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $autoStorage: any;
+  }
+}
+
+declare module 'vue/types/options' {
+  interface ComponentOptions<V extends _Vue> {
+    autoStorage?: any;
+  }
+}
+
+declare function install(Vue: typeof _Vue): void;
+
+declare const _default: {
+  install: typeof install;
+};
+export default _default;
